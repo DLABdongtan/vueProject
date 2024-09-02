fee1007db2c8a93f8577abe8085f93366244e449
dlabdongtan <dongtan.edu@daddyslab.com>
2024-09-02 21:28:14 +0900

    props예제
    props예제 코드

---
+++ props1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- 자식 컴포넌트에 props로 데이터를 전달 -->
+    <GreetingCard name="Alice" message="Welcome to Vue.js!" />
+  </div>
+</template>
+
+<script>
+import GreetingCard from './components/GreetingCard.vue';
+
+export default {
+  components: {
+    GreetingCard
+  }
+};
+</script>
---
+++ props1/src/components/GreetingCard.vue
@@ -0,0 +1,42 @@
+<!-- <template>
+    <div>
+      <h1>Hello, {{ name }}!</h1>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      name: String,
+      message: String
+    }
+  };
+  </script> -->
+
+  <template>
+    <div>
+      <h1>Hello, {{ name }}!</h1>
+      <p>{{ message }}</p>
+      <p>{{ age }} years old</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      message: {
+        type: String,
+        default: "This is a default message"
+      },
+      age: {
+        type: Number,
+        default: 25
+      }
+    }
+  };
+  </script>
