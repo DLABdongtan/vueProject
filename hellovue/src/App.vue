3a89de0907177991f6b9b53c581b49cbe58776c9
dlabdongtan <dongtan.edu@daddyslab.com>
2024-09-02 20:46:43 +0900

    파일 변경
    hello world출력 및 샘플코드 작성

--- hellovue/src/App.vue
+++ hellovue/src/App.vue
@@ -1,26 +1,20 @@
+<!-- App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello world</h1>
+  <div>
+    <FirstComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FirstComponent from '@/components/FirstComponents.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstComponent
   }
-}
+};
 </script>
+<style scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
